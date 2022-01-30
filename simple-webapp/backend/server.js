ee9d0a63ceb401fceac227b13ba8765cf9cec662
sakar345 <sakar345@yahoo.com>
2022-01-30 08:16:56 -0500

    fixing greet issue

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const url = require("url");
-//const greet = require("./greet");
+const greet = require("./greet");
 const greeting = greet(name);
 
 const server = http.createServer((req, res) => {
